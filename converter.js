4c56386b539f8e4eda2d9d9638a8b7e95fb2d9ad
rbrt457 <rob.step457@ya.ru>
2021-04-22 18:55:38 +0300

    add romeConverter

--- converter.js
+++ converter.js
@@ -1,44 +1,20 @@
-let symVal = [[1, 'I'],[5,'V'],[10,'X'],[50,'L'],[100,'C'],[500, 'D'],[1000, 'M']]
-const sv = new Map(symVal)
-let convertNumb
-let ost
-let arr = []
-
-
-// function solution(number){
-//     convertNumb = number
-//     //Конвертация до 3
-//     if(convertNumb <= 3){
-//         for (let i = 0; i < convertNumb; i++){
-//             arr.push(sv.get(1))}}
-//     //Конвертация от 4 до 8
-//     if(convertNumb == 4){
-//         arr.push(sv.get(1))
-//         arr.push(sv.get(5))
-//     }
-//     if(convertNumb == 5){
-//         arr.push(sv.get(5))
-//
-//     }
-//     if(convertNumb > 5 && convertNumb < 9){
-//         ost = convertNumb - 5;
-//         arr.push(sv.get(5))
-//
-//     }
-//     let convert = arr.join('')
-//     console.log(convert)
-// }
-function solution(num) {
-    if (!num) return "";
-    let rezult = "";
-    let n = font_ar.length - 1;
-    while (num > 0) {
-        if (num >= font_ar[n]) {
-            rezult += font_rom[n];
-            num -= font_ar[n];
-        }
-        else n--;
+function convert(roman){
+    let symVal = ['I','V','X','L','C','D','M']
+    let value = [1,5,10,50,100,500,1000]
+    roman = roman.toUpperCase().split('')
+    let latNumb = []
+    for (let i = 0; i < roman.length; i++){
+        latNumb.push(value[symVal.indexOf(roman[i])])
     }
-    return rezult;
+    let result = latNumb.reduce(function (x, y){
+        if(x < y){
+           return y - x
+        }
+        else{
+            return x + y
+        }
+    })
+    console.log(result)
+    return result
 }
-solution(6)
+convert('xi')
